449a48b954b6ddf1e90078d84289ce6d1a476f26
badagaud <vishnugaud315@gmail.com>
2025-02-16 09:39:47 +0530

    add create and delete file with uploads

--- php/create.php
+++ php/create.php
@@ -1,3 +1,24 @@
 <?php
 include('../database/config.php');
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $upload = "../uploads/";
+    $image = $_FILES['image']['name'];
+    $tmp_name = $_FILES['image']['tmp_name'];
+    $full_path = $upload.$image;
+    $add_data = "insert into add_data (name,image,email) values('$name','$full_path','$email')";
+    $result = $conn->query($add_data);
+    if($result){
+        if(move_uploaded_file($tmp_name,$full_path)){
+            echo "uploaded file";
+        }
+        else{
+            echo "not upload file";
+        }
+    }
+    else{
+        echo "not inserted data into data base";
+    }
+}
 ?>
--- php/view.php
+++ php/view.php
@@ -1,3 +1,48 @@
-<?php
-include('../database/config.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>image</th>
+            <th>delete</th>
+            <th>update</th>
+        </tr>
+        <?php
+        include('../database/config.php');
+
+        $data = "select * from add_data";
+        $result = $conn->query($data);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id  =    $row['id'];
+                $demail =  $row['email'];
+                $dname =   $row['name'];
+                $dimage =   $row['image'];
+        ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['name'];  ?></td>
+                    <td><?php echo $row['email'];  ?></td>
+                    <td><?php echo $row['image'];  ?></td>
+                    <td><a href="../html/delete.php?id=<?php echo $row['id'];?>">Delete</a></td>
+                    <td><a href="../html/update.php?id=<?php echo $row['id'];?>">update</a></td>
+                </tr>
+
+        <?php
+            }
+        }
+        ?>
+    </table>
+</body>
+
+</html>
